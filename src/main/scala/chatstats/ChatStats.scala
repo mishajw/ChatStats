d6e9104627f6ff8826953d2dbb973ad4d9ff50ce
Misha Wagner <mishajw@gmail.com>
2016-10-02 15:53:16 +0100

    Refactor stats extraction

--- src/main/scala/chatstats/ChatStats.scala
+++ src/main/scala/chatstats/ChatStats.scala
@@ -5,26 +5,46 @@ import scala.collection.mutable.ArrayBuffer
 object ChatStats {
 
   val seekingGroup = "nicholas artrayo alex hoagy connie tayo charlie sam sophie emma jasmine wilfrid" split " "
+  val path: String = "src/main/resources/messages_xml.htm"
 
-  val allMessages = ArrayBuffer[Message]()
+  def main(args: Array[String]): Unit = {
+    val messages = loadMessages()
 
-  def main(args: Array[String]) {
-    val messageCollector = new MessageCollector("src/main/resources/messages_xml.htm", messageHandler)
+    perMemberCounts(messages)
+    mostPopularWords(messages)
+  }
+
+  def loadMessages() = {
+    val allMessages = ArrayBuffer[Message]()
+
+    def messageHandler(message: Message): Unit = {
+      if (seekingGroup forall message.threadMembers.toSeq.contains) {
+        allMessages += message
+      }
+    }
+
+    val messageCollector = new MessageCollector(path, messageHandler)
 
     messageCollector.run()
 
+    allMessages
+  }
+
+  def perMemberCounts(messages: Seq[Message]) = {
     val counts =
-      allMessages
-        .groupBy({ m => m.header.name })
-        .map( { case (sender, ms) => (sender, ms.length) }).toSeq
-        .sortBy(-_._2)
+    messages
+      .groupBy({ m => m.header.name })
+      .map( { case (sender, ms) => (sender, ms.length) }).toSeq
+      .sortBy(-_._2)
 
     println(counts.mkString("\n"))
   }
 
-  def messageHandler(message: Message): Unit = {
-    if (seekingGroup forall message.threadMembers.toSeq.contains) {
-      allMessages += message
-    }
+  def mostPopularWords(messages: ArrayBuffer[Message]): Unit = {
+    messages.flatMap(_.contents.toLowerCase().split("[^A-Za-z0-9']+"))
+        .groupBy(identity)
+        .map({ case (w, ws) => (w, ws.length) }).toSeq
+        .sortBy(-_._2)
+        .foreach(println)
   }
 }
